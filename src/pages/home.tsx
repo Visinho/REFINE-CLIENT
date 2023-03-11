291a1ea8b5c08167449c0409685864811a7a80e5
Visinho <Elvisvisinho@rocketmail.com>
2023-03-11 17:57:56 +0100

    Designed the dashboard header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import { useList } from '@pankod/refine-core';
+import React from "react";
+import { useList } from "@pankod/refine-core";
 import {
   PieChart,
   PropertyReferrals,
   TotalRevenue,
   PropertyCard,
-  TopAgent
-} from "components"
-import { typography, Box, Stack } from '@pankod/refine-mui';
+  TopAgent,
+} from "components";
+import { Typography, Box, Stack } from "@pankod/refine-mui";
 
 const home = () => {
   return (
     <Box>
-      
-    </Box>
-  )
-}
+      <Typography fontSize={25} fontWeight={700} color="#111420">
+        Dashboard
+      </Typography>
 
-export default home
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart title="Properties for sale" value={684} series={[175, 25]} colors={["#275be8", "#c4e8ef"]}/>
+        <PieChart title="Properties for rent" value={550} series={[60, 40]} colors={["#275be8", "#c4e8ef"]}/>
+        <PieChart title="Total Customers" value={584} series={[75, 25]} colors={["#275be8", "#c4e8ef"]}/>
+        <PieChart title="Properties for Cities" value={555} series={[175, 25]} colors={["#275be8", "#c4e8ef"]}/>
+      </Box>
+    </Box>
+  );
+};
+
+export default home;
