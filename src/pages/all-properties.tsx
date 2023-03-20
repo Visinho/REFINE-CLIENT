c0bab11a621803060acebe79cee8cadc219f528d
Visinho <Elvisvisinho@rocketmail.com>
2023-03-20 02:02:27 +0100

    fetched all properties, displayed, and styled them

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Add } from '@mui/icons-material';
-import { useList } from '@pankod/refine-core';
+import { useTable } from '@pankod/refine-core';
 import { Box, Stack, Typography } from "@pankod/refine-mui";
 import { useNavigate } from '@pankod/refine-react-router-v6';
 import { PropertyCard, CustomButton } from 'components';
 
 const AllProperties = () => {
   const navigate = useNavigate();
+
+  const {
+    tableQueryResult: { data, isLoading, isError }
+  } = useTable();
+
+  // console.log(data);
+
+  const allProperties = data?.data ?? [];
+
+  if(isLoading) return <Typography>Loading...</Typography>
+  if(isError) return <Typography>Error...</Typography>
+
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
@@ -20,6 +32,18 @@ const AllProperties = () => {
           color="#fcfcfc"
           icon={<Add />}/>
       </Stack>
+      <Box sx={{ display: "flex", flexWrap: "wrap", gap: 3}}>
+        {allProperties.map((property) => (
+            <PropertyCard 
+              key={property._id}
+              id={property._id}
+              title={property.title}
+              price={property.price}
+              photo={property.photo}
+              location={property.location}
+            />
+        ))}
+      </Box>
     </Box>
   )
 }
