8e12d095462c7b2df4a124c668d650df48834bd6
Visinho <Elvisvisinho@rocketmail.com>
2023-05-07 07:47:32 +0100

    Added the changes for edit and delete design

--- src/pages/property-details.tsx
+++ src/pages/property-details.tsx
@@ -1,163 +1,3 @@
-// import React from "react";
-// import { Typography, Box, Stack } from "@pankod/refine-mui";
-// import { useDelete, useGetIdentity, useShow } from "@pankod/refine-core";
-// import { useParams, useNavigate } from "@pankod/refine-react-router-v6";
-// import {
-//   ChatBubble,
-//   Delete,
-//   Edit,
-//   Phone,
-//   Place,
-//   Star,
-// } from "@mui/icons-material";
-
-// import { CustomButton } from "components";
-
-// const PropertyDetails = () => {
-//   const navigate = useNavigate();
-//   const { data: user } = useGetIdentity();
-//   const { id } = useParams();
-//   const { mutate } = useDelete();
-//   const { queryResult } = useShow();
-
-//   const { data, isLoading, isError } = queryResult;
-
-//   const propertyDetails = data?.data ?? {};
-
-//   if (isLoading) {
-//     return <div>Loading...</div>;
-//   }
-//   if (isError) {
-//     return <div>Something went wrong!</div>;
-//   }
-
-//   // const isCurrentUser = user.email === propertyDetails.creator.email;
-
-//   // const handleDeleteProperty = () => {
-//   //     const response = confirm(
-//   //         "Are you sure you want to delete this property?",
-//   //     );
-//   //     if (response) {
-//   //         mutate(
-//   //             {
-//   //                 resource: "properties",
-//   //                 id: id as string,
-//   //             },
-//   //             {
-//   //                 onSuccess: () => {
-//   //                     navigate("/properties");
-//   //                 },
-//   //             },
-//   //         );
-//   //     }
-//   // };
-
-//   return (
-//     <Box
-//       borderRadius="15px"
-//       padding="20px"
-//       bgcolor="#fcfcfc"
-//       width="fit-content"
-//     >
-//       <Typography fontSize={25} fontWeight={700} color="#11142d">
-//         Property Details
-//       </Typography>
-//       <Box
-//         mt="20px"
-//         display="flex"
-//         flexDirection={{ xs: "column", lg: "row" }}
-//         gap={4}
-//       >
-//         <Box flex={1} maxWidth={764}>
-//           <img
-//             src={propertyDetails.photo}
-//             alt={propertyDetails.title}
-//             height={546}
-//             style={{ objectFit: "cover", borderRadius: "10px" }}
-//             className="property_details-img"
-//           />
-//           <Box mt="15px">
-//             <Stack
-//               direction="row"
-//               justifyContent="space-between"
-//               alignItems="center"
-//               flexWrap="wrap"
-//             >
-//               <Typography
-//                 fontSize={18}
-//                 fontWeight={500}
-//                 color="#11142d"
-//                 textTransform="capitalize"
-//               >
-//                 {propertyDetails.propertyType}
-//               </Typography>
-//               <Box>
-//                 {[1, 2, 3, 4, 5].map((star) => (
-//                   <Star key={`star-${star}`} sx={{ color: "#f2c94c" }} />
-//                 ))}
-//               </Box>
-//             </Stack>
-//             <Stack
-//               direction="row"
-//               justifyContent="space-between"
-//               alignItems="center"
-//               flexWrap="wrap"
-//             >
-//               <Box>
-//                 <Typography
-//                   fontSize={22}
-//                   fontWeight={600}
-//                   color="#11142d"
-//                   textTransform="capitalize"
-//                 >
-//                   {propertyDetails.title}
-//                 </Typography>
-
-//                 <Stack mt={0.5} direction="row" alignItems="center" gap={0.5}>
-//                   <Place sx={{ color: "#808191" }} />
-//                   <Typography fontSize={14} color="#808191">
-//                     {propertyDetails.location}
-//                   </Typography>
-//                 </Stack>
-//               </Box>
-//               <Box>
-//                 <Typography
-//                   fontSize={16}
-//                   fontWeight={600}
-//                   mt="10px"
-//                   color="#11142D"
-//                 >
-//                   Price
-//                 </Typography>
-//                 <Stack direction="row" alignItems="flex-end" gap={1}>
-//                   <Typography fontSize={25} fontWeight={700} color="#475BE8">
-//                     ${propertyDetails.price}
-//                   </Typography>
-//                   <Typography fontSize={14} color="#808191" mb={0.5}>
-//                     for one day
-//                   </Typography>
-//                 </Stack>
-//               </Box>
-//             </Stack>
-//             <Stack mt="25px" direction="column" gap="10px">
-//                             <Typography fontSize={18} color="#11142D">
-//                                 Description
-//                             </Typography>
-//                             <Typography fontSize={14} color="#808191">
-//                                 {propertyDetails.description}
-//                             </Typography>
-//                         </Stack>
-
-//           </Box>
-//         </Box>
-
-//       </Box>
-//     </Box>
-//   );
-// };
-
-// export default PropertyDetails;
-
 import { Typography, Box, Stack } from "@mui/material";
 import { useDelete, useGetIdentity, useShow } from "@pankod/refine-core";
 import { useParams, useNavigate } from "react-router-dom";
@@ -203,24 +43,24 @@ const PropertyDetails = () => {
 
     const isCurrentUser = user.email === propertyDetails.creator.email;
 
-    // const handleDeleteProperty = () => {
-    //     const response = confirm(
-    //         "Are you sure you want to delete this property?",
-    //     );
-    //     if (response) {
-    //         mutate(
-    //             {
-    //                 resource: "properties",
-    //                 id: id as string,
-    //             },
-    //             {
-    //                 onSuccess: () => {
-    //                     navigate("/properties");
-    //                 },
-    //             },
-    //         );
-    //     }
-    // };
+    const handleDeleteProperty = () => {
+        const response = confirm(
+            "Are you sure you want to delete this property?",
+        );
+        if (response) {
+            mutate(
+                {
+                    resource: "properties",
+                    id: id as string,
+                },
+                {
+                    onSuccess: () => {
+                        navigate("/properties");
+                    },
+                },
+            );
+        }
+    };
 
     return (
         <Box
@@ -435,7 +275,7 @@ const PropertyDetails = () => {
                             flexWrap="wrap"
                             gap={2}
                         >
-                            {/* <CustomButton
+                            <CustomButton
                                 title={!isCurrentUser ? "Message" : "Edit"}
                                 backgroundColor="#475BE8"
                                 color="#FCFCFC"
@@ -450,8 +290,8 @@ const PropertyDetails = () => {
                                         );
                                     }
                                 }}
-                            /> */}
-                            {/* <CustomButton
+                            />
+                            <CustomButton
                                 title={!isCurrentUser ? "Call" : "Delete"}
                                 backgroundColor={
                                     !isCurrentUser ? "#2ED480" : "#d42e2e"
@@ -462,7 +302,7 @@ const PropertyDetails = () => {
                                 handleClick={() => {
                                     if (isCurrentUser) handleDeleteProperty();
                                 }}
-                            /> */}
+                            />
                         </Stack>
                     </Stack>
 
