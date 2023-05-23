11db4ba8ab5e8a6008072fb0ff9af8ee8dc0914d
Visinho <Elvisvisinho@rocketmail.com>
2023-05-23 01:59:41 +0100

    Made a change to the data provider endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function App() {
 
       //save user to MongoDB
       if(profileObj){
-        const response = await fetch("http://localhost:8080/api/v1/users", {
+        const response = await fetch("https://refine-api-backend.onrender.com/api/v1/users", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
@@ -126,7 +126,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://refine-api-backend.onrender.com")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
